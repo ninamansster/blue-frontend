80945d0157b7243ca7f5b8b1d60f844385519042
Nina Månsson <ninamansster@gmail.com>
2020-03-27 22:02:39 +0100

    logg out takes you back

--- src/pages/components/Logout.js
+++ src/pages/components/Logout.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export const LogoutUser = () => {
   // If this is active, the function runs without pressing the button
   // const accessToken = localStorage.removeItem('accessToken')
+  const history = useHistory()
 
   const onLoggedOut = (event) => {
     event.preventDefault()
     window.localStorage.removeItem('accessToken')
+    history.push('/')
   }
 
   return (
